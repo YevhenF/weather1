56c92ce75e59f5d01612098fb65cd00dedb3bc0d
yevhenf <footbool.geka@gmail.com>
2025-12-17 21:35:41 +0200

    renaming and creatingtest the weather file

--- weather.py
+++ weather.py
@@ -2,4 +2,4 @@ city = str(input('Введите название своего города: '))
 import requests
 response = requests.get(f'https://wttr.in/{city}?format=j1')
 data = response.json()
-print('темпуратура в городе ', city,data["current_condition"][0]["temp_C"], ' градусов')
+print('Темпуратура в городе ', city,data["current_condition"][0]["temp_C"], ' градусов')
